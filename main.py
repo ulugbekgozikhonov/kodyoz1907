a927354bd5a1a56370dc970602b6d2e197077605
unknown <ulugbekgozixonov@gmail.com>
2025-02-18 17:50:03 +0500

    test api yaratildi main .py da

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app.db.session import get_db
-from app.models.user import User
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-async def demo_user(db: AsyncSession = Depends(get_db)):
-	user = User(
-		username="devkhon",
-		email="ketmon@gmail.com",
-		password="asdfasdf",
-		firstname="Asdfg",
-		lastname="asdfgasdf",
-		gender="male"
-	)
-
-	db.add(user)
-	await db.commit()
-	await db.refresh(user)
-	return user
+@app.get("/test")
+async def root():
+	return {"message": "Hello World"}
